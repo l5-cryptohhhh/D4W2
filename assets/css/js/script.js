63e4ba2a09ef3e4e9e7a153f31eacbcce3dba87c
l5-cryptohhhh <nunziatamanuel5@gmail.com>
2026-05-14 14:16:54 +0200

    D4W2

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -77,5 +77,40 @@ for (let i = 0; i < 8; i++) {
     count.innerHTML += `<li>${contatore()}<li>`;
 }
 
+let myName = 'Manuel Nunziata';
+console.log(myName.includes('Nunziata'));
+console.log(myName.includes('Leo'));
+console.log(myName.slice(6, 10));
+console.log(myName.slice(6));
+
+
+const myNameArray = myName.split(' ');
+console.table(myNameArray);
+
+let myNameJoined = myNameArray.join().replaceAll(',', ' ');
+console.log(myNameJoined);
+
+const today = new Date();
+console.log(today);
+
+
+const myBirthday = new Date(2004, 7, 3);
+console.log(myBirthday);
+
+let myAbs = Math.abs(3.15);
+console.log(myAbs);
+
+let myMaxNumber = Math.max(12, 21, 3);
+console.log(myMaxNumber);
+
+let myCasualNumber = Math.floor(Math.random() * 11);
+console.log(myCasualNumber);
+
+
+
+
+
+
+
 
 
